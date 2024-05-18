21ef1e401f12402fa8ac93038532547fd06cbc45
PatrickAlexandre <PatrickAlexandre@users.noreply.github.com>
2024-05-18 20:49:58 +0200

    MyDNAHeritage

--- scripts.js
+++ scripts.js
@@ -97,85 +97,6 @@
                 return Math.abs(ageDate.getUTCFullYear() - 1970);
             }
 
-            const ctx = document.getElementById('myChart').getContext('2d');
-            const myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: [
-                        'Investing in Stocks (08:00)', 'Networking (09:00)', 'Personal Development (10:00)', 
-                        'Fitness Routine (11:00)', 'Reading (12:00)', 'Managing Businesses (13:00)', 
-                        'Traveling (14:00)', 'Learning New Skills (15:00)', 'Real Estate Investments (16:00)', 
-                        'Meditation (17:00)', 'Philanthropy (18:00)', 'High-End Dining (19:00)', 
-                        'Luxury Shopping (20:00)', 'Golfing (21:00)', 'Collecting Art (22:00)', 
-                        'Attending Conferences (23:00)', 'Advising Startups (00:00)', 'Yachting (01:00)', 
-                        'Skiing (02:00)', 'Spa Days (03:00)', 'Driving Luxury Cars (04:00)', 
-                        'Charity Events (05:00)', 'Exclusive Parties (06:00)'
-                    ],
-                    datasets: [{
-                        label: '/ DAY',
-                        data: [12, 19, 15, 10, 8, 20, 14, 16, 30, 6, 18, 21, 11, 7, 13, 5, 4, 3, 2, 1, 0, 1, 2],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-
             function updateLifeExpectancy(age, chromosome) {
                 const maxLifeExpectancyFemale = 82;
                 const maxLifeExpectancyMale = 78;
@@ -341,10 +262,14 @@
 
                 const info = mbtiInfo[selectedType];
                 const infoHtml = `
-                    <p>${info.demographic} ${info.nickname} ${info.subtype}</p>
-                    <div><small><b class="w3-text-gray">${info.functionPrimary}</b></small></div>
-                    <div><small><b class="w3-text-gray">${info.functionAux}</b></small></div>
-
+                    <p>${info.subtype}</p>
+                    <b>${info.nickname}</b>
+                    <hr>
+                    <div>${info.demographic}</div>
+                    <small>
+                        <div>${info.functionPrimary}</div>
+                        <div>${info.functionAux}</div>
+                    </small>
                 `;
                 document.getElementById('mbti-info').innerHTML = infoHtml;
             });
